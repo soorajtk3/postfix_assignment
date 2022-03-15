44e3dd8257c3e88daac1707146148b69fe52714b
sooraj <sooraj@hamon.in>
2022-03-15 10:29:04 +0530

    coverage reached to 100%

--- test_postfix.py
+++ test_postfix.py
@@ -11,6 +11,16 @@ def test_postfix():
     assert ret == 14
 
 
+def test_postfixsub():
+    ret = postfix_program.postfix("25-")
+    assert ret == 3
+
+
+def test_postfixdiv():
+    ret = postfix_program.postfix("22/")
+    assert ret == 1
+
+
 def testonly_operator():
     ret = postfix_program.postfix("/*-+*-/-/-+")
     assert ret == "error"
